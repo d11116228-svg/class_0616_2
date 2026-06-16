78501c1732aa76b36a9792f64672d0659d27474d
d11116228-svg <d11116228@gs.takming.edu.tw>
2026-06-16 16:12:48 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Student
+{
+    public int Mid;
+    public int Final;
+    public string[] Names;
+
+    public Student()
+    {
+        Names = new string[3];
+        Names[0] = "AAA";
+        Names[1] = "BBB";
+        Names[2] = "CCC";
+    }
+
+    public string this[int pos]
+    {
+        get
+        {
+            return this.Names[pos];
+        }
+        set
+        {
+            this.Names[pos] = value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace Class_0616_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Console.WriteLine(s1[0]);
+            Console.WriteLine(s1[1]);
+            Console.WriteLine(s1[2]);
+        }
+    }
+}
